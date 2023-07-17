01c45979e02387cd931bbd229314fdc33bf08faf
Scala Steward <scala_steward@virtuslab.com>
2023-07-17 03:10:30 +0000

    Reformat with scalafmt 3.7.10
    Executed command: scalafmt --non-interactive

--- src/main/scala/smithy4s/todo/package.scala
+++ src/main/scala/smithy4s/todo/package.scala
@@ -5,11 +5,11 @@ import example.todo.{Order, Title, Todo, TodoDescription}
 package object todo {
   implicit class TodoOps(val todo: Todo) extends AnyVal {
     def update(
-                title: Option[Title],
-                description: Option[TodoDescription],
-                order: Option[Order],
-                completed: Option[Boolean]
-              ): Todo =
+        title: Option[Title],
+        description: Option[TodoDescription],
+        order: Option[Order],
+        completed: Option[Boolean]
+    ): Todo =
       todo.copy(
         title = title.getOrElse(todo.title),
         description = description.orElse(todo.description),
